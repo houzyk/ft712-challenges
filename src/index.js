a1eb93287c67b625257643775b22c5f48017a9a2
houzairkoussa <houzairmk@icloud.com>
2021-10-31 22:49:52 +0400

    reorganized js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Navbar JS
+
+const navbar = document.querySelector(".navbar");
+const navWrapper = document.querySelector(".nav-wrapper")
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= window.innerHeight / 4) {
+    navbar.classList.add("navbar-white");
+    navWrapper.classList.add("top");
+  } else {
+    navbar.classList.remove("navbar-white");
+    navWrapper.classList.remove("top");
+  }
+});
