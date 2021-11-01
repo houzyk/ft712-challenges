cb31bcb26acfb48e431de028bd0ee11f68d4f1c8
houzairkoussa <houzairmk@icloud.com>
2021-11-01 10:33:03 +0400

    added js for navbar and seasonal photos

--- javascript/index.js
+++ javascript/index.js
@@ -21,49 +21,67 @@ form.addEventListener("submit", (event) => {
 });
 
 // Dropdown JS
-// const dropdowns = document.querySelectorAll('.dropbtn');
-// const dropdown1 = document.querySelector('.dropdown-content-left');
-// const dropdown2 = document.querySelectorAll('.dropdown-content-right')[0];
-// const dropdown3 = document.querySelectorAll('.dropdown-content-right')[1];
+const dropdowns = document.querySelectorAll('.dropdown');
+const dropdown1 = document.querySelector('.dropdown-content-left');
+const dropdown2 = document.querySelectorAll('.dropdown-content-right')[0];
+const dropdown3 = document.querySelectorAll('.dropdown-content-right')[1];
 
-// dropdowns[0].addEventListener("mouseover", () => {
-//   dropdown1.style.display= "flex";
-// });
-// dropdowns[0].addEventListener("mouseleave", () => {
-//   dropdown1.addEventListener("mouseover", () => {
-//     dropdown1.style.display= "flex";
-//   });
-//   dropdown1.addEventListener("mouseleave", () => {
-//     dropdown1.style.display= "none";
-//   });
-//   dropdown1.style.display= "none";
-// });
+dropdowns[0].addEventListener("mouseover", () => {
+  dropdown1.style.display= "flex";
+});
+dropdown1.addEventListener("mouseover", () => {
+    dropdown1.style.display= "flex";
+});
+dropdowns[0].addEventListener("mouseleave", () => {
+  dropdown1.style.display= "none";
+});
+dropdown1.addEventListener("mouseleave", () => {
+    dropdown1.style.display= "none";
+});
 
-// dropdowns[1].addEventListener("mouseover", () => {
-//   dropdown2.style.display= "inherit";
-// });
-// dropdown2.addEventListener("mouseover", () => {
-//     dropdown2.style.display= "inherit";
-//   });
-// dropdown2.addEventListener("mouseleave", () => {
-//     dropdown2.style.display= "none";
-//   });
+dropdowns[1].addEventListener("mouseover", () => {
+  dropdown2.style.display= "inherit";
+});
+dropdown2.addEventListener("mouseover", () => {
+    dropdown2.style.display= "inherit";
+});
+dropdowns[1].addEventListener("mouseleave", () => {
+    dropdown2.style.display= "none";
+});
+dropdown2.addEventListener("mouseleave", () => {
+    dropdown2.style.display= "none";
+});
 
-// setTimeout(() => {
-//   dropdowns[1].addEventListener("mouseleave", () => {
-//     dropdown2.style.display= "none";
-//   });
-// }, 500);
+dropdowns[2].addEventListener("mouseover", () => {
+  dropdown3.style.display= "inherit";
+});
+dropdown3.addEventListener("mouseover", () => {
+  dropdown3.style.display= "inherit";
+  });
+dropdowns[2].addEventListener("mouseleave", () => {
+  dropdown3.style.display= "none";
+});
+dropdown3.addEventListener("mouseleave", () => {
+  dropdown3.style.display= "none";
+});
 
-// dropdowns[2].addEventListener("mouseover", () => {
-//   dropdown3.style.display= "inherit";
-// });
-// dropdowns[2].addEventListener("mouseleave", () => {
-//   dropdown3.style.display= "none";
-//   dropdown3.addEventListener("mouseover", () => {
-//     dropdown3.style.display= "inherit";
-//   });
-//   dropdown3.addEventListener("mouseleave", () => {
-//     dropdown3.style.display= "none";
-//   });
-// });
+// Seasons Photo JS
+const seasonImages = document.querySelector('.dropdown-images');
+const seasons = document.querySelectorAll('.seasons');
+const summer = seasons[0];
+const winter = seasons[1];
+const spring = seasons[2];
+const fall = seasons[3];
+
+summer.addEventListener("mouseover", () => {
+  seasonImages.innerHTML = `<a href="#"> <img src="https://filmdaily.co/wp-content/uploads/2021/06/summer.jpg" alt="summer"/> </a>`;
+});
+winter.addEventListener("mouseover", () => {
+  seasonImages.innerHTML = `<a href="#"> <img src="https://www.almanac.com/sites/default/files/image_nodes/winter_sunrise-2.jpg" alt="winter"/> </a>`;
+});
+fall.addEventListener("mouseover", () => {
+  seasonImages.innerHTML = `<a href="#"> <img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/MA_00031467_hrit6x.jpg" alt="fall"/> </a>`;
+});
+spring.addEventListener("mouseover", () => {
+  seasonImages.innerHTML = `<a href="#"> <img src="https://images.unsplash.com/photo-1602094891528-97ea074d30b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="spring"/> </a>`;
+});
